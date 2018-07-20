a50634758ef068fd89cfbae5f03c749b5768f339
Ailon <510_529678737_Ailon@git.cloud.tencent.com>
2018-07-20 15:51:53 +0800

    post 模板分离

---
+++ data/posts-data.js
@@ -0,0 +1,33 @@
+var local_database = [{
+    'date': 'Sep 15 2016',
+    'title': 'title',
+    'imgSrc': '/images/post/crab.png',
+    'avatar': '/images/avatar/1.png',
+    'content': 'content',
+    'reading': '12',
+    'collection': '90',
+  },
+  {
+    'date': 'Sep 18 2019',
+    'title': 'title',
+    'imgSrc': '/images/post/cat.png',
+    'aavataruthor_img': '/images/avatar/2.png',
+    'content': 'content',
+    'reading': '15',
+    'collection': '80',
+  },
+
+  {
+    'date': 'Sep 18 2019',
+    'title': 'title',
+    'imgSrc': '/images/post/bl.png',
+    'aavataruthor_img': '/images/avatar/2.png',
+    'content': 'content',
+    'reading': '15',
+    'collection': '80',
+  }
+]
+
+module.exports = {
+  postList: local_database
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,5 @@
+var postsData = require('../../data/posts-data.js')
+
 Page({
 
   /**
@@ -11,78 +13,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var posts_content = [{
-        'date': 'Sep 15 2016',
-        'title': 'title',
-        'post_img': '/images/post/crab.png',
-        'author_img': '/images/avatar/1.png',
-        'content': 'content',
-        'view_num': '12',
-        'collect_num': '90',
-      },
-      {
-        'date': 'Sep 18 2019',
-        'title': 'title',
-        'post_img': '/images/post/cat.png',
-        'author_img': '/images/avatar/2.png',
-        'content': 'content',
-        'view_num': '15',
-        'collect_num': '80',
-      }
-    ]
-
+    
     this.setData({
-      posts_content: posts_content
+      posts_content: postsData.postList
     })
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
 
   }
 })
