47e62f592b1ad6b267110ec72d4ed2029687c6d7
AilonHuang <529678737@qq.com>
2018-07-22 13:02:26 +0800

    post收藏和取消收藏

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -14,9 +14,41 @@ Page({
    */
   onLoad: function (options) {
     var postId = options.id;
+    this.setData({
+      postId:postId
+    })
     var postData = postsData.postList[postId];
     this.setData({
       postData: postData
+    })
+
+    try {
+      var postsCollected = wx.getStorageSync('posts_collected')
+      if (postsCollected) {
+        var postCollected = postsCollected[postId]
+        this.setData({
+          collected: postCollected
+        })
+      } else {
+        postsCollected = {};
+        postCollected = postsCollected[postId] = false;
+        wx.setStorageSync('posts_collected', postsCollected)
+      }
+    } catch (e) {
+      // Do something when catch error
+      console.log(e);
+    }
+  },
+
+  onCollectionTap: function(event) {
+    var postsCollected = wx.getStorageSync('posts_collected');
+    var postCollected = postsCollected[this.data.postId]; 
+    console.log(postCollected);
+    postCollected = !postCollected;
+    postsCollected[this.data.postId] = postCollected;
+    wx.setStorageSync('posts_collected', postsCollected)
+    this.setData({
+      collected: postCollected
     })
   }
 })
